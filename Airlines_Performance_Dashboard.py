73b79e464c4515bc1c866f5190f8d6f911960c4f
Folongton <Vasyl.Zhepikov@gmail.com>
2021-07-13 23:45:16 -0500

    Commi

--- Airlines_Performance_Dashboard.py
+++ Airlines_Performance_Dashboard.py
@@ -43,7 +43,6 @@ def compute_data_choice_2(df):
     return avg_car, avg_weather, avg_NAS, avg_sec, avg_late
 
 
-# Application layout
 app.layout = html.Div(children=[
                                     html.H1('US Domestic Airline Flights Performance',
                                             style={
@@ -117,9 +116,6 @@ app.layout = html.Div(children=[
                                                        ) 
                                               ])
                                 ])
-# Callback function definition
-# TASK4: Add 5 ouput components
-# Enter your code below. Make sure you have correct formatting.
 
 @app.callback( [Output(component_id='plot1', component_property='children'),
                 Output(component_id='plot2', component_property='children'),
@@ -128,7 +124,7 @@ app.layout = html.Div(children=[
                 Output(component_id='plot5', component_property='children'),],
                [Input(component_id='input-type', component_property='value'),
                 Input(component_id='input-year', component_property='value')],
-               # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
+               # Holding output state till user enters all the form information. In this case, it will be chart type and year
                [State("plot1", 'children'), State("plot2", "children"),
                 State("plot3", "children"), State("plot4", "children"),
                 State("plot5", "children")
@@ -141,20 +137,15 @@ def get_graph (chart, year, children1, children2, c3, c4, c5):
     df =  airline_data[airline_data['Year']==int(year)]
 
     if chart == 'OPT1':
-        # Compute required information for creating graph from the data
+        
         bar_data, line_data, div_data, map_data, tree_data = compute_data_choice_1(df)
         
-        # Number of flights under different cancellation categories
         bar_fig = px.bar(bar_data, x='Month', y='Flights', color='CancellationCode', title='Monthly Flight Cancellation')
         
-        # TASK5: Average flight time by reporting airline
-        # Enter your code below. Make sure you have correct formatting.
         line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline', title='Average monthly flight time (minutes) by airline')
         
-        # Percentage of diverted airport landings per reporting airline
         pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='% of flights by reporting airline')
         
-        # REVIEW5: Number of flights flying from each state using choropleth
         map_fig = px.choropleth(map_data,  # Input data
                 locations='OriginState', 
                 color='Flights',  
@@ -164,18 +155,14 @@ def get_graph (chart, year, children1, children2, c3, c4, c5):
                 range_color=[0, map_data['Flights'].max()]) 
         map_fig.update_layout(
                 title_text = 'Number of flights from origin state', 
-                geo_scope='usa') # Plot only the USA instead of globe
+                geo_scope='usa')
         
-        # TASK6: Number of flights flying to each state from each reporting airline
-        # Enter your code below. Make sure you have correct formatting.
         tree_fig = px.treemap(tree_data, path=['DestState', 'Reporting_Airline'], 
                                         values='Flights',
                                         color='Flights',
                                         color_continuous_scale='RdBu',
                                         title='Flight count by airline to destination state')
         
-        
-        # REVIEW6: Return dcc.Graph component to the empty division
         return [dcc.Graph(figure=tree_fig), 
                 dcc.Graph(figure=pie_fig),
                 dcc.Graph(figure=map_fig),
@@ -183,11 +170,8 @@ def get_graph (chart, year, children1, children2, c3, c4, c5):
                 dcc.Graph(figure=line_fig)
                 ]
     else:
-        # REVIEW7: This covers chart type 2 and we have completed this exercise under Flight Delay Time Statistics Dashboard section
-        # Compute required information for creating graph from the data
         avg_car, avg_weather, avg_NAS, avg_sec, avg_late = compute_data_choice_2(df)
         
-        # Create graph
         carrier_fig = px.line(avg_car, x='Month', y='CarrierDelay', color='Reporting_Airline', title='Average carrrier delay time (minutes) by airline')
         weather_fig = px.line(avg_weather, x='Month', y='WeatherDelay', color='Reporting_Airline', title='Average weather delay time (minutes) by airline')
         nas_fig = px.line(avg_NAS, x='Month', y='NASDelay', color='Reporting_Airline', title='Average NAS delay time (minutes) by airline')
@@ -201,6 +185,5 @@ def get_graph (chart, year, children1, children2, c3, c4, c5):
                 dcc.Graph(figure=late_fig)]
 
 
-# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
